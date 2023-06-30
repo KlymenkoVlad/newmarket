9b534e1fc87b81b6a725b0e31864e96d486b654f
Vlad Klymenko <klymenvlad@gmail.com>
2023-06-30 21:27:11 +0300

    fix to deploy-1

--- frontend/utils/baseUrl.js
+++ frontend/utils/baseUrl.js
@@ -1,4 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV !== "production" ? "http://localhost:3001" : "https://P";
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3001"
+    : process.env.NODE_ENV;
 
 module.exports = baseUrl;
