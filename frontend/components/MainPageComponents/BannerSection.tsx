8fca60d3cc7ce0059e29114d274dc2fb4b31e39f
Vlad Klymenko <klymenvlad@gmail.com>
2023-07-06 23:11:52 +0300

    small improving

--- frontend/components/MainPageComponents/BannerSection.tsx
+++ frontend/components/MainPageComponents/BannerSection.tsx
@@ -12,13 +12,18 @@ const BannerSection = ({ slideItems }: BannerSectionProps) => {
   });
 
   return (
-    <div className="flex my-16">
-      <NavMenu />
-
-      <div className="m-auto h-[500px] w-[892px] py-4 px-4 relative group">
-        <Slider slideItems={slideItems} />
+    <>
+      <div className="grid items-center w-full ms:h-8 h-12 bg-red-600 text-white text-center lg:mt-12 ms:mt-4 mt-16">
+        <p>If something not load just wait, backend is launching...</p>
       </div>
-    </div>
+      <div className="flex  mb-16">
+        <NavMenu />
+
+        <div className="m-auto h-[500px] w-[892px] py-4 px-4 relative group">
+          <Slider slideItems={slideItems} />
+        </div>
+      </div>
+    </>
   );
 };
 
