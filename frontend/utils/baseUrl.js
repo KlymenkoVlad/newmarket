472fafc73dbf39a53a1459951a39f74b26d89db6
Vlad Klymenko <klymenvlad@gmail.com>
2023-07-19 18:03:26 +0300

    frontend fix

--- frontend/utils/baseUrl.js
+++ frontend/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3001"
-    : "https://newmarket-backend.onrender.com";
+    : "https://dull-red-bat-slip.cyclic.app/";
 
 module.exports = baseUrl;
